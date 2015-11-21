e3099a34801c6df871e7e2a55c000e8cfd80e9c2
Lindsay Laidlaw <lindsay@passionplatformmedia.com>
2015-11-21 15:24:30 -0400

    Add basic spawner and shark prefab.

---
+++ Assets/Scripts/Shark.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shark : MonoBehaviour {
+	public float speed = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localPosition += Vector3.left * Time.deltaTime * speed;
+	}
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+	public GameObject prefab;
+	public float spawnTime;
+
+	float time;
+	float globalTime;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (time > spawnTime) {
+			Spawn ();
+			spawnTime = 2.0f + UnityEngine.Random.value * 2.0f;
+
+			time = 0;
+		}
+
+		transform.localPosition = new Vector3 (transform.localPosition.x, 
+		                                  Mathf.Sin (globalTime * 4 * Mathf.PI),
+		                                  transform.localPosition.z);
+
+		globalTime += Time.deltaTime;
+		time += Time.deltaTime;
+
+	}
+
+	void Spawn() {
+		var instance = GameObject.Instantiate (prefab);
+		instance.transform.position = transform.position;
+	}
+}
